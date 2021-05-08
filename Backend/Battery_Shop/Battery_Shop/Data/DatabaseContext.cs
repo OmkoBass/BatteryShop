372b2d68984c7fbc0b27941be9470667f69b50ef
OmkoBass <omkob@hotmail.com>
2021-05-08 21:45:34 +0200

    Added database context

---
+++ Backend/Battery_Shop/Battery_Shop/Data/DatabaseContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Battery_Shop.Models;
+
+namespace Battery_Shop.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<BatteryShop> BatteryShops { get; set; }
+        public DbSet<Battery> Batteries { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Storage> Storages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BatteryShop>(BatteryShop => BatteryShop.HasIndex(b => b.Name).IsUnique());
+            modelBuilder.Entity<Battery>().Property(b => b.Warrant).HasDefaultValueSql("getdate()");
+        }
+    }
+}
