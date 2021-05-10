da107c2d5a234a1642c6973a0712324417a2e134
OmkoBass <omkob@hotmail.com>
2021-05-10 14:23:49 +0200

    Employee controller

---
+++ Backend/Battery_Shop/Battery_Shop/Controllers/EmployeeController.cs
@@ -0,0 +1,102 @@
+﻿using AutoMapper;
+using Battery_Shop.Data;
+using Battery_Shop.Dtos;
+using Battery_Shop.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Battery_Shop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        public EmployeeController(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var Employees = await _unitOfWork.IEmployeeRepo.GetAllEmployees();
+
+            return Ok(Employees);
+        }
+
+        [HttpGet(":id")]
+        public async Task<IActionResult> Get(int Id)
+        {
+            var Employee = await _unitOfWork.IEmployeeRepo.GetEmployee(Id);
+
+            if(Employee != null)
+            {
+                return Ok(Employee);
+            }
+
+            return NotFound(new { Message = $"Employee with Id:{Id} not found!" });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] AddEmployeeDto Employee)
+        {
+            if(ModelState.IsValid)
+            {
+                var AddedEmployee = _mapper.Map<Employee>(Employee);
+
+                await  _unitOfWork.IEmployeeRepo.AddEmployee(AddedEmployee);
+                await _unitOfWork.Complete();
+
+                return Ok(AddedEmployee);
+            }
+
+            return BadRequest(new { Message = "Invalid info!" });
+        }
+
+        [HttpPut(":id")]
+        public async Task<IActionResult> Put(int Id, [FromBody] AddEmployeeDto Employee)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(new { Message = "Invalid info!" });
+            }
+
+            var OldEmployee = await _unitOfWork.IEmployeeRepo.GetEmployee(Id);
+
+            if(OldEmployee == null)
+            {
+                return NotFound(new { Message = $"Employee with Id:{Id} not found." });
+            }
+
+            _mapper.Map<AddEmployeeDto, Employee>(Employee, OldEmployee);
+
+            await _unitOfWork.Complete();
+
+            return Ok(OldEmployee);
+        }
+
+        [HttpDelete(":id")]
+        public async Task<IActionResult> Delete(int Id)
+        {
+            var Employee = await _unitOfWork.IEmployeeRepo.GetEmployee(Id);
+
+            if(Employee != null)
+            {
+                return NotFound(new { Message = $"Employee with Id:{Id} not found." });
+            }
+
+            _unitOfWork.IEmployeeRepo.DeleteEmployee(Employee);
+            await _unitOfWork.Complete();
+
+            return Ok(Employee);
+        }
+    }
+}
