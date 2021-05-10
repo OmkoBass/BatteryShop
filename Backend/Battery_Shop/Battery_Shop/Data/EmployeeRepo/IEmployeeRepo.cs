be3851d823dd58120587166881453cf765e29e0d
OmkoBass <omkob@hotmail.com>
2021-05-10 14:22:45 +0200

    Employee Repo

---
+++ Backend/Battery_Shop/Battery_Shop/Data/EmployeeRepo/IEmployeeRepo.cs
@@ -0,0 +1,17 @@
+﻿using Battery_Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Battery_Shop.Data.EmployeeRepo
+{
+    public interface IEmployeeRepo
+    {
+        public Task<List<Employee>> GetAllEmployees();
+        public Task<Employee> GetEmployee(int EmployeeId);
+        public Task<bool> AddEmployee(Employee Employee);
+        public bool UpdateEmployee(Employee Employee);
+        public bool DeleteEmployee(Employee Employee);
+    }
+}
---
+++ Backend/Battery_Shop/Battery_Shop/Data/EmployeeRepo/MockEmployeeRepo.cs
@@ -0,0 +1,46 @@
+﻿using Battery_Shop.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Battery_Shop.Data.EmployeeRepo
+{
+    public class MockEmployeeRepo : IEmployeeRepo
+    {
+        private readonly DatabaseContext _context;
+        public MockEmployeeRepo(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> AddEmployee(Employee Employee)
+        {
+            await _context.Employees.AddAsync(Employee);
+            return true;
+        }
+
+        public bool DeleteEmployee(Employee Employee)
+        {
+            _context.Employees.Remove(Employee);
+            return true;
+        }
+
+        public async Task<List<Employee>> GetAllEmployees()
+        {
+            return await _context.Employees.ToListAsync();
+        }
+
+        public async Task<Employee> GetEmployee(int EmployeeId)
+        {
+            return await _context.Employees.FindAsync(EmployeeId);
+        }
+
+        public bool UpdateEmployee(Employee Employee)
+        {
+            _context.Employees.Update(Employee);
+            return true;
+        }
+    }
+}
