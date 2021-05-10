10b69527d5238d623a98a37000f48b29701ec8d8
OmkoBass <omkob@hotmail.com>
2021-05-10 14:22:54 +0200

    Unit of work

---
+++ Backend/Battery_Shop/Battery_Shop/Data/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using Battery_Shop.Data.AuthRepo;
+using Battery_Shop.Data.EmployeeRepo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Battery_Shop.Data
+{
+    public interface IUnitOfWork
+    {
+        public IEmployeeRepo IEmployeeRepo { get; set; }
+        public IAuthRepo IAuthRepo { get; set; }
+        Task <bool> Complete();
+    }
+}
