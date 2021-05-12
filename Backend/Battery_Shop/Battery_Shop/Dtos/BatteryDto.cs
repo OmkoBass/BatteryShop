5d55cab5261d0f189de0ba360bebfd815f433de5
OmkoBass <omkob@hotmail.com>
2021-05-13 01:18:43 +0200

    Dtos

---
+++ Backend/Battery_Shop/Battery_Shop/Dtos/BatteryDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Battery_Shop.Dtos
+{
+    public class BatteryDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+
+        public int Life { get; set; }
+
+        public DateTime Warrant { get; set; }
+
+        public bool Sold { get; set; }
+
+        public int StorageId { get; set; }
+        public int? CustomerId { get; set; }
+    }
+}
---
+++ Backend/Battery_Shop/Battery_Shop/Dtos/StorageDto.cs
@@ -0,0 +1,19 @@
+﻿using Battery_Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Battery_Shop.Dtos
+{
+    public class StorageDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<BatteryDto> Batteries { get; set; }
+
+        public int BatterShopId { get; set; }
+    }
+}
