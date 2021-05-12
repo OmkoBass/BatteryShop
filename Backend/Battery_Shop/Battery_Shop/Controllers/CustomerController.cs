41f02b075434acb281073b8487143ad61a9e4375
OmkoBass <omkob@hotmail.com>
2021-05-12 15:16:18 +0200

    Authoriation added

--- Backend/Battery_Shop/Battery_Shop/Controllers/CustomerController.cs
+++ Backend/Battery_Shop/Battery_Shop/Controllers/CustomerController.cs
@@ -2,6 +2,8 @@
 using Battery_Shop.Data;
 using Battery_Shop.Dtos;
 using Battery_Shop.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -10,6 +12,7 @@ using System.Threading.Tasks;
 
 namespace Battery_Shop.Controllers
 {
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     [Route("api/[controller]")]
     [ApiController]
     public class CustomerController : ControllerBase
