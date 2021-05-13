3bdcf8515e3da3c70e5b74664fe8ff43e06d5a96
OmkoBass <omkob@hotmail.com>
2021-05-13 21:37:48 +0200

    Refactoring

--- Frontend/battery-shop/src/Views/SalesPage.js
+++ Frontend/battery-shop/src/Views/SalesPage.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react';
 
-import { getBatteriesByBatteryShop } from "../utils";
+import { getBatteriesByBatteryShop, batteryColumns } from "../utils";
 
-import {Table, message, Typography, Input, Button} from "antd";
+import {Table, message, Input, Button} from "antd";
 
-import {DollarTwoTone, ThunderboltTwoTone} from "@ant-design/icons";
 import SellBatteryModal from "../Components/SellBatteryModal";
 
 export default function SalesPage() {
@@ -28,41 +27,6 @@ export default function SalesPage() {
         })();
     }, []);
 
-    const columns = [
-        {
-            title: "Id",
-            dataIndex: "id",
-        },
-        {
-            title: "Name",
-            dataIndex: "name",
-        },
-        {
-            title: () => (
-                <Typography.Text>
-                    Life <ThunderboltTwoTone twoToneColor="#ffec3d" />
-                </Typography.Text>
-            ),
-            dataIndex: "life",
-            render: (data) => <Typography.Text>{data}</Typography.Text>,
-        },
-        {
-            title: () => (
-                <Typography.Text>
-                    Price <DollarTwoTone twoToneColor="#52c41a" />
-                </Typography.Text>
-            ),
-            dataIndex: "price",
-            sorter: (a, b) => a.price - b.price,
-        },
-        {
-            title: 'Sold',
-            dataIndex: 'sold',
-            render: (text) => (<Typography.Text> {text ? 'SOLD' : 'Available'} </Typography.Text>),
-            sorter: (a, b) => a.sold - b.sold
-        }
-    ];
-
     const rowSelection = {
         onSelect: (record) => {
             if(!record.sold) {
@@ -74,6 +38,14 @@ export default function SalesPage() {
     return <div>
         <Button type="primary" disabled={!selectedBattery} onClick={() => setSellBatteryModal(true)}>
             Sell
+        </Button>
+
+        <Button type="primary" style={{ marginLeft: '1em' }}>
+            Intervention
+        </Button>
+
+        <Button type="primary" style={{ marginLeft: '1em' }}>
+            Warranties
         </Button>
 
         <Input.Search
@@ -101,7 +73,7 @@ export default function SalesPage() {
             rowKey={"id"}
             loading={loading}
             dataSource={searchedBatteries.length === 0 ? batteries : searchedBatteries}
-            columns={columns}
+            columns={batteryColumns}
         />
 
         <SellBatteryModal
