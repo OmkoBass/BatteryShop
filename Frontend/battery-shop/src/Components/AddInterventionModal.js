afb69b9454b53f3e587a8e8a8ccdad7798446052
OmkoBass <omkob@hotmail.com>
2021-05-14 16:05:32 +0200

    Added Intervention Modal

---
+++ Frontend/battery-shop/src/Components/AddInterventionModal.js
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+
+import {Modal, Form, Input, AutoComplete, Button, InputNumber, Typography} from "antd";
+
+export default function AddInterventionModal({ visible, batteries, handleClose }) {
+    const [battery, setBattery] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const batteryNames = batteries.map(battery => {
+        return {
+            value: battery.name
+        }
+    });
+
+    const handleOnFinish = async (values) => {
+        setLoading(true);
+        try {
+
+        } catch {
+
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    return <Modal
+        title={'Add Intervention'}
+        visible={visible}
+        onCancel={handleClose}
+        footer={false}
+        closable={false}
+    >
+        <Form onFinish={handleOnFinish}>
+            <Form.Item
+                name='Battery'
+                rules={[
+                    {
+                        required: true,
+                        message: 'Battery is required!'
+                    }
+                ]}
+            >
+                <AutoComplete
+                    size='large'
+                    onSelect={value => setBattery(batteries.find(battery => battery.name === value))}
+                    placeholder='Search for available batteries'
+                    options={batteryNames}
+                />
+            </Form.Item>
+
+            <Form.Item
+                name="location"
+                rules={[
+                    {
+                        required: true,
+                        message: "Username is required!",
+                    },
+                    {
+                        min: 3,
+                        message: "Atleast 3 charachters!",
+                    },
+                    {
+                        max: 64,
+                        message: "Maximum 16 charachters!",
+                    },
+                ]}
+            >
+                <Input size="large" placeholder="Location" />
+            </Form.Item>
+
+            <Typography.Text>
+                Price is calculated automatically by adding 5000 to the current battery price
+            </Typography.Text>
+
+            <InputNumber size="large" min={0} value={battery?.price + 5000} placeholder="Price" />
+
+            <Form.Item style={{ marginTop: '1em' }}>
+                <Button type="primary" htmlType="submit" loading={loading}>
+                    Add
+                </Button>
+
+                <Button
+                    loading={loading}
+                    type="danger"
+                    onClick={() => handleClose()}
+                    style={{ marginLeft: "1em" }}
+                >
+                    Close
+                </Button>
+            </Form.Item>
+        </Form>
+    </Modal>
+}
