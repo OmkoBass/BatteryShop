f876ebd4de7672a0354ca16ac4986ebc131b87bf
OmkoBass <omkob@hotmail.com>
2021-06-01 17:52:24 +0200

    Added battery info modal

--- Frontend/battery-shop/src/Views/Storage.js
+++ Frontend/battery-shop/src/Views/Storage.js
@@ -1,81 +1,103 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 
-import { useParams, useHistory } from "react-router-dom";
+import {useParams, useHistory} from "react-router-dom";
 
-import { Table, message, Button, Input, PageHeader } from "antd";
+import {Table, message, Button, Input, PageHeader} from "antd";
 
 import AddBatteryModal from "../Components/AddBatteryModal";
 
-import { getStorage, batteryColumns } from "../utils";
+import {getStorage, batteryColumns} from "../utils";
+import BatteryInfoModal from "../Components/BatteryInfoModal";
 
 export default function Storage() {
-  const [batteries, setBatteries] = useState([]);
-  const [searchedBatteries, setSearchedBatteries] = useState([]);
-  const [loading, setLoading] = useState(true);
+    const [batteries, setBatteries] = useState([]);
+    const [searchedBatteries, setSearchedBatteries] = useState([]);
+    const [batteryModal, setBatteryModal] = useState(false);
+    const [selectedBattery, setSelectedBattery] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-  const [addEmployeeVisible, setAddEmployeeVisible] = useState(false);
-  const history = useHistory();
+    const [addEmployeeVisible, setAddEmployeeVisible] = useState(false);
+    const history = useHistory();
 
-  const { id } = useParams();
+    const {id} = useParams();
 
-  useEffect(() => {
-    (async () => {
-      try {
-        const { data } = await getStorage(id);
+    useEffect(() => {
+        (async () => {
+            try {
+                const {data} = await getStorage(id);
 
-        setBatteries(data.batteries);
-      } catch {
-        message.error("Something went wrong!");
-      } finally {
-        setLoading(false);
-      }
-    })();
-  }, [id]);
+                setBatteries(data.batteries);
+            } catch {
+                message.error("Something went wrong!");
+            } finally {
+                setLoading(false);
+            }
+        })();
+    }, [id]);
 
-  return (
-    <div>
-      <PageHeader
-        onBack={() => history.goBack()}
-        title={`Storage ${id}`}
-        subTitle="Only articles of this storage will be displayed"
-        style={{ marginLeft: "-2em", marginTop: "-2em" }}
-      />
-      <Button type="primary" onClick={() => setAddEmployeeVisible(true)}>
-        Add Battery
-      </Button>
+    return (
+        <div>
+            <PageHeader
+                onBack={() => history.goBack()}
+                title={`Storage ${id}`}
+                subTitle="Only articles of this storage will be displayed"
+                style={{marginLeft: "-2em", marginTop: "-2em"}}
+            />
+            <Button type="primary" onClick={() => setAddEmployeeVisible(true)}>
+                Add Battery
+            </Button>
 
-      <Input.Search
-        style={{ marginTop: "1em" }}
-        placeholder={"Enter name of the battery"}
-        allowClear
-        onSearch={(value) => {
-          if (value) {
-            setSearchedBatteries(
-              batteries.filter((battery) => battery.name.includes(value))
-            );
-          } else {
-            setSearchedBatteries([]);
-          }
-        }}
-      />
+            <Input.Search
+                style={{marginTop: "1em"}}
+                placeholder={"Enter name of the battery"}
+                allowClear
+                onSearch={(value) => {
+                    if (value) {
+                        setSearchedBatteries(
+                            batteries.filter((battery) => battery.name.includes(value))
+                        );
+                    } else {
+                        setSearchedBatteries([]);
+                    }
+                }}
+            />
 
-      <Table
-        style={{ marginTop: "1em" }}
-        title={() => "Batteries"}
-        rowKey={"id"}
-        loading={loading}
-        dataSource={
-          searchedBatteries.length === 0 ? batteries : searchedBatteries
-        }
-        columns={batteryColumns}
-      />
+            <Table
+                style={{marginTop: "1em"}}
+                title={() => "Batteries"}
+                rowKey={"id"}
+                loading={loading}
+                onRow={(record) => {
+                    return {
+                        onClick: () => {
+                            setBatteryModal(true);
+                            setSelectedBattery(record);
+                        },
+                    };
+                }}
+                dataSource={
+                    searchedBatteries.length === 0 ? batteries : searchedBatteries
+                }
+                columns={batteryColumns}
+            />
 
-      <AddBatteryModal
-        visible={addEmployeeVisible}
-        onClose={() => setAddEmployeeVisible(false)}
-        storageId={id}
-        addBatteryToTable={(battery) => setBatteries([...batteries, battery])}
-      />
-    </div>
-  );
+            <AddBatteryModal
+                visible={addEmployeeVisible}
+                onClose={() => setAddEmployeeVisible(false)}
+                storageId={id}
+                addBatteryToTable={(battery) => setBatteries([...batteries, battery])}
+            />
+
+            <BatteryInfoModal
+                storage={true}
+                removeBatteryFromTable={() => {
+                    setBatteries(batteries.filter(battery => battery.id !== selectedBattery.id));
+                    setSelectedBattery(null);
+                }}
+                visible={batteryModal}
+                handleClose={() => setBatteryModal(false)}
+                battery={selectedBattery}
+            />
+        </div>
+    );
 }
