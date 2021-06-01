5ec3ebabb6c5597daed4276c8fad0c61ddeb9270
OmkoBass <omkob@hotmail.com>
2021-06-01 17:31:41 +0200

    Added the ability to add new storages and it's implementation

--- Frontend/battery-shop/src/Views/StoragePage.js
+++ Frontend/battery-shop/src/Views/StoragePage.js
@@ -2,13 +2,15 @@ import React, { useState, useEffect } from "react";
 
 import { useHistory } from "react-router-dom";
 
-import { Table, message } from "antd";
+import {Table, message, Typography, Button} from "antd";
 
 import { getStorages } from "../utils";
+import StorageModal from "../Components/StorageModal";
 
 export default function StoragePage() {
   const [storages, setStorages] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [storageModal, setStorageModal] = useState(false);
 
   const history = useHistory();
 
@@ -39,7 +41,18 @@ export default function StoragePage() {
 
   return (
     <div>
+      <Typography.Title level={4}>
+        Select a storage, view it's batteries and Add or Delete them.
+      </Typography.Title>
+
+      <Button type={'primary'}
+              onClick={() => setStorageModal(true)}
+      >
+        Add Storage
+      </Button>
+
       <Table
+        style={{ marginTop: "1em" }}
         title={() => "Storages"}
         rowKey="id"
         loading={loading}
@@ -51,6 +64,12 @@ export default function StoragePage() {
           };
         }}
       />
+
+        <StorageModal
+            visible={storageModal}
+            handleClose={() => setStorageModal(false)}
+            addStorageToTable={storage => setStorages([...storages, storage])}
+        />
     </div>
   );
 }
